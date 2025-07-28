7fc07ab146415ea050c8e689b0c5dfb8ba209afe
Trent Houliston <trent@houliston.me>
2025-07-28 12:47:11 +1000

    Clean up

--- build.rs
+++ build.rs
@@ -1,24 +1,30 @@
 use std::env;
 use std::fs::File;
-use std::path::Path;
+use std::io::Write;
+use std::path::PathBuf;
 
 fn main() {
-    // Create an empty memory.x file to satisfy the include in the main linker script
-    let out_dir = env::var("OUT_DIR").unwrap();
-    let dest_path = Path::new(&out_dir).join("memory.x");
-    File::create(&dest_path).unwrap();
+    // Put `memory.x` in our output directory and ensure it's
+    // on the linker search path.
+    let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
+    File::create(out.join("memory.x"))
+        .unwrap()
+        .write_all(include_bytes!("memory.x"))
+        .unwrap();
 
     // Add the output directory to the linker search path so `link.x` can find the empty `memory.x`
-    println!("cargo:rustc-link-search={}", out_dir);
+    println!("cargo:rustc-link-search={}", out.display());
+
+    // println!("cargo:rustc-link-arg-bins=-Tmemory.x");
+
+    // By default, Cargo will re-run a build script whenever
+    // any file in the project changes. By specifying `memory.x`
+    // here, we ensure the build script is only re-run when
+    // `memory.x` is changed.
+    println!("cargo:rerun-if-changed=memory.x");
 
     // Set linker flags for all binaries
     println!("cargo:rustc-link-arg-bins=--nmagic");
-
-    // Use the appropriate memory file based on the binary being built
-    println!("cargo:rustc-link-arg-bin=boot=-Tmemory-bootloader.x");
-    println!("cargo:rerun-if-changed=memory-bootloader.x");
-    println!("cargo:rustc-link-arg-bin=nusense-rs=-Tmemory-app.x");
-    println!("cargo:rerun-if-changed=memory-app.x");
 
     // Main linker script, uses the memory layout from above
     println!("cargo:rustc-link-arg-bins=-Tlink.x");
